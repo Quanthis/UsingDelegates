e245da75a0c3990b9d1b97b2cc4f14261e8dc8b1
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2019-11-07 16:09:21 +0100

    Example Added

--- UsingDelegates/Program.cs
+++ UsingDelegates/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading;
+using static System.Console;
+using System.Numerics;
 
 namespace UsingDelegates
 {
     class Program
     {
+        public delegate void ThreadMaker(object ob);
+        public static void Silnia(int n)
+        {
+            BigInteger wynik = 1;
+            for (BigInteger i = 1; i < 10000; i++)
+            {
+                wynik *= i;
+            }
+        }
+
+
+
+
+        public delegate void Del(string s);
+        public static void DelMethod(string s)
+        {
+            WriteLine(s);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Del handler = DelMethod;
+            handler("print sth");
         }
     }
 }
